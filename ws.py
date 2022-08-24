4ba049590978becb367b1586b84f4e2ee95270d8
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-08-23 23:44:28 -0400

    server

--- ws.py
+++ ws.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             'tools.staticdir.dir': './public'
         }
     }
-
+    cherrypy.server.socket_host = '0.0.0.0'
     cherrypy.engine.subscribe('start', setup_database)
     cherrypy.engine.subscribe('stop', cleanup_database)
 
